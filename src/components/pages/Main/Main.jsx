1bb44f503056a998e7aac6bc37201860f31133cb
frexxx-7 <borzenkokiril9@gmail.com>
2023-02-01 17:43:40 +0300

    add about company in main page

--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -3,7 +3,7 @@ import classes from './Main.module.scss'
 function Main() {
   return (
     <main className={classes.main}>
-      <div className={classes.content}>
+      <div className={classes.cardFullContent}>
         <div className={classes.arrow}>
           <i className="fa-solid fa-chevron-left" />
         </div>
@@ -17,7 +17,7 @@ function Main() {
                 </h2>
                 <p>Для каждого из направлений мы собрали типовые продукты.
                   Мы рекомендуем связаться с нашими специалистами чтобы подобрать правильный продукт.</p>
-                  <button className={classes.button}>Узнать больше</button>
+                <button className={classes.button}>Узнать больше</button>
               </div>
             </div>
 
@@ -37,7 +37,48 @@ function Main() {
           <i className="fa-solid fa-chevron-right" />
         </div>
       </div>
-      
+
+      <div className={classes.aboutCompany}>
+        <div className={classes.triangeTop}>
+
+        </div>
+
+        <div className={classes.aboutCompanyContent}>
+          <h2>О компании</h2>
+
+          <div className={classes.contentAbout}>
+            <div className={classes.imageAbout}>
+              <img src="/Mask group.png" alt="" />
+            </div>
+
+            <div className={classes.textAbout}>
+              <h6>«Quality Oil» – ваш надёжный партнёр в поставках
+                смазочных материалов с 1994 г.</h6>
+
+              <p>ООО «Ойл-Форби» начала свою деятельность в 1994 году с поставок автозапчастей и аксессуаров для автомобилей.</p>
+
+              <p>
+                С тех пор мы всё также остаемся верны убеждениям, что у дистрибьютора на первом месте  должны стоять хорошая цена, контроль качества, а уже потом прыбль
+              </p>
+
+              <p>Почти 30 лет наша компания обеспечивает людей качественным и надежными запчастями, маслом и фильтрами</p>
+              <div className={classes.aboutDataNumber}>
+                <p className={classes.aboutDataNumberP}><span className={classes.aboutDataNumberInfo}>19</span> <span className={classes.aboutDataNumberMoreInfo}>тыс</span><br /> Поставок</p>
+                <p className={classes.aboutDataNumberP}><span className={classes.aboutDataNumberInfo}>29</span><br /> Лет На рынке</p>
+                <p className={classes.aboutDataNumberP}><span className={classes.aboutDataNumberInfo}>1,2</span> <span className={classes.aboutDataNumberMoreInfo}>млн</span><br /> Довольных клиентов</p>
+                <p className={classes.aboutDataNumberP}><span className={classes.aboutDataNumberInfo}>7</span><br /> Стран присутствия</p>
+              </div>
+
+              <button className={classes.aboutCompanyButton}>Больше о компании</button>
+            </div>
+
+          </div>
+        </div>
+
+        <div className={classes.triangeBottom}>
+
+        </div>
+      </div>
     </main>
   )
 }
